e144c1ca206ee50430178935fbcc53dfc34f608d
devi <devi.nurjanah@students.amikom.ac.id>
2018-12-11 11:19:06 +0700

    New project Calculator

---
+++ src/lab/amikom/Calculator.java
@@ -0,0 +1,14 @@
+package lab.amikom;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				int a = 10;
+				int b = 5;
+				
+				int add = a + b;
+				System.out.println("Hasil Penjumlahan = "+add);
+			}
+
+}
